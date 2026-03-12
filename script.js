f8d6fa2cb8fb53d4fafca0899d81e3248c0fd2b9
Samarth-creater77 <samarthjmpatel77@gmail.com>
2026-03-12 19:35:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function submitExam(){
-document.getElementById("result").innerHTML="Exam Submitted Successfully";
+function submitExam() {
+    // This creates a popup message to confirm submission
+    alert("Exam Submitted Successfully!");
+    
+    // This is the magic line that moves the user to the next page
+    window.location.href = "result.html"; 
 }
